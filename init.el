e83c5926b1e14b863dc77b53fa53da47301090bb
Zhaoyu Luo <luozhaoyu90@gmail.com>
2015-10-13 20:40:54 -0500

    finish emacs formatting

--- init.el
+++ init.el
@@ -1,25 +1,53 @@
 ; basics
 
-(global-hl-line-mode 1)
 (add-hook 'find-file-hook (lambda () (linum-mode 1)))
 (setq column-number-mode t)
 (setq scroll-step 1)
 (setq scroll-margin 7)
+(setq scroll-conservatively 10000)
+(setq redisplay-dont-pause t)
+(show-paren-mode 1)
 
-; installed
+
+; auto install
 ; evil go-mode cider clojure-mode python-mode
 ; M-x package-list-package
 ; M-x package-menu-execute
-
+; list the packages you want installed
 (require 'package)
-(push '("marmalade" . "http://marmalade-repo.org/packages/")
-            package-archives )
+(setq package-list '(
+		     evil go-mode cider clojure-mode python-mode yasnippet
+		     rainbow-delimiters highlight-symbol hl-sexp
+		    ))
+; list the repositories containing them
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+; activate all the packages (in particular autoloads)
 (package-initialize)
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+; installed package configurations
 (require 'evil)
 (evil-mode 1)
 
 (require 'yasnippet)
 (yas-global-mode 1)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(add-hook 'prog-mode-hook #'hl-sexp-mode)
+
+(require 'highlight-symbol)
+(setq highlight-symbol-idle-delay 0.5)
+(add-hook 'find-file-hook #'highlight-symbol-mode)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
