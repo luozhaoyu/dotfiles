6734c18668d6d2544493581f6d2829c7a798c724
zhaoyu luo <zhaoyu.luo@onepeloton.com>
2022-04-07 15:36:42 -0400

    spark path

--- config.fish
+++ config.fish
@@ -6,4 +6,8 @@ if status --is-interactive
     source $HOME/codes/dotfiles/company.fish
 end
 
-set PATH $PATH $HOME/go/bin/ $HOME/local/bin $HOME/.cargo/bin $HOME/Library/Python/3.8/bin
+set -x SPARK_HOME /opt/spark
+set -x JAVA_HOME /usr/local/opt/openjdk@11/
+set PATH $PATH $HOME/go/bin/ $HOME/local/bin $HOME/.cargo/bin $HOME/Library/Python/3.8/bin /opt/spark/bin/
+fish_add_path /usr/local/opt/openjdk/bin
+fish_add_path /usr/local/opt/openjdk@11/bin
